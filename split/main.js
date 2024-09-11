561a1fed3124360a5d9365568aa00ce12588fb3a
pavan b <pavannttf29@gmail.com>
2024-09-11 16:49:40 +0530

    fix: modify the split function 🚀

--- split/main.js
+++ split/main.js
@@ -4,17 +4,18 @@ String.prototype.mySplit = function (delimiter) {
   if (delimiter === "") {
     return Array.from(string);
   }
-  function helper(str) {
+  function helper(str, i) {
+    if (i >= string.length) return;
     const index = str.indexOf(delimiter);
     if (index !== -1) {
       console.log(index);
       result.push(str.slice(0, index));
-      helper(str.slice(index + delimiter.length));
+      helper(str.slice(index + delimiter.length), index + delimiter.length);
     } else {
       result.push(str.toString());
     }
   }
-  helper(string);
+  helper(string, 0);
   return result;
 };
 
