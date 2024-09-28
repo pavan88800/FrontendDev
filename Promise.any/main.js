409974eb3592a95a24493999e210a7ff4af74c11
pavan b <pavannttf29@gmail.com>
2024-09-28 15:03:07 +0530

    fix: any promise  🚀

--- Promise.any/main.js
+++ Promise.any/main.js
@@ -25,3 +25,25 @@ function any(promises) {
     });
   });
 }
+
+// Promise 2
+function any(promises) {
+  // your code here
+  let isError = 0;
+  const errorList = [];
+  return new Promise((resolve, reject) => {
+    promises.forEach((el, index) => {
+      Promise.resolve(el)
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((err) => {
+          errorList[index] = err;
+          isError++;
+          if (promises.length === isError) {
+            reject(new AggregateError(errorList, "All promises were rejected"));
+          }
+        });
+    });
+  });
+}
