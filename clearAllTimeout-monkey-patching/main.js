271d3f07ee361f97c2a5bca788a8c146ee0bc024
pavan b <pavannttf29@gmail.com>
2025-06-27 21:05:08 +0530

    fix: add note in the Deeper object question

--- clearAllTimeout-monkey-patching/main.js
+++ clearAllTimeout-monkey-patching/main.js
@@ -16,3 +16,26 @@ function clearAllTimeout() {
     }
   };
 }
+
+(function () {
+  const originalSetTimeout = window.setTimeout;
+  const originalClearTimeout = window.clearTimeout;
+  const timeoutIds = new Set();
+  window.setTimeout = function (cb, delay, ...args) {
+    if (typeof cb !== "function") {
+      throw new TypeError("Callback must be a function");
+    }
+    let id = originalSetTimeout(cb, delay, ...args);
+    timeoutIds.add(id);
+    return id;
+  };
+
+  window.clearTimeout = function (id) {
+    timeoutIds.delete(id);
+    originalClearTimeout(id);
+  };
+  window.clearAllTimeout = function () {
+    timeoutIds.forEach((id) => originalClearTimeout(id));
+    timeoutIds.clear();
+  };
+})();
